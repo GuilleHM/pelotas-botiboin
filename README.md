3643df913c0d9028cc53de3d18439c4d623bff91
GuilleHM <guillehm1@outlook.com>
2020-04-29 13:35:05 +0200

    Confirmación Inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pelotas Botiboin
+
+### pelotas de colores que rebotan contra las paredes del navegador
+
+Ejercicio planteado en la web de la _Mozilla Development Network_, incluyendo varias modificaciones para personalizar el resultado final del proyecto: un fondo y una cabecera distintas, las pelotas no incluyen estela (para facilitar la visualización) y en caso de colisión aparecen/desaparecen y cambian de tamaño y color.
+
+Este mini proyecto tiene como objetivo fundamental practicar el uso del modelo de clases para la definición de objetos (en lugar del menos intuitivo modelo basado en funciones constructoras y prototipos), introducido en la especificación del lenguaje JavaScript ECMAScript2015 (a.k.a. ES6), y la API Canvas para la representación de figuras 2D.
---
+++ dist/main.js
@@ -0,0 +1,113 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min)) + min;
+    return num;
+}
+
+class Ball {
+
+    constructor(x, y, velX, velY, color, size) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+        this.color = color;
+        this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+
+    update() {
+        if ((this.x + this.size) >= width) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.velX = -(this.velX);
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.velY = -(this.velY);
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.velY = -(this.velY);
+        }
+
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    collisionDetect() {
+        for (let j = 0; j < balls.length; j++) {
+            if (!(this === balls[j])) {
+                const dx = this.x - balls[j].x;
+                const dy = this.y - balls[j].y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + balls[j].size) {
+                    if (this.color === 'rgba(0, 39, 39, 1)') {
+                        this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+                    } else {
+                        this.color = 'rgba(0, 39, 39, 1)';
+                    }
+                    if (balls[j].color === 'rgba(0, 39, 39, 1)') {
+                        balls[j].color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+                    } else {
+                        balls[j].color = 'rgba(0, 39, 39, 1)';
+                    }
+                    this.size = random(10, 40);
+                    balls[j].size = random(10, 40);
+                }
+            }
+        }
+    }
+
+}
+
+var balls = [];
+
+while (balls.length < 20) {
+    const size = random(10, 20);
+    let ball = new Ball(
+        // ball position always drawn at least one ball width
+        // away from the edge of the canvas, to avoid drawing errors
+        random(0 + size, width - size),
+        random(0 + size, height - size),
+        random(-4, 4),
+        random(-4, 4),
+        'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+        size
+    );
+
+    balls.push(ball);
+}
+
+function loop() {
+    ctx.fillStyle = 'rgba(0, 39, 39, 1)';
+    ctx.fillRect(0, 0, width, height);
+
+    for (let i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+        balls[i].collisionDetect();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
